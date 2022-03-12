43caffb4e5711ae9f3f4f257387d294ea6685382
koushik-murali <m.koushik1995@gmail.com>
2022-03-12 12:27:14 +0530

    revert to simple node app deploy

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const article = require('./articles')   
+const dotenv = require('dotenv');
+const Mongoose = require('mongoose');
+const serverless = require('serverless-http')
+dotenv.config();
+Mongoose.connect(process.env.CONSTRING, {useNewUrlParser: true, useUnifiedTopology: true,})
+
+app.set('view engine', 'ejs')
+
+app.get('/', async (req, res) =>{
+    try{
+        const articles = await article.find()
+        res.render('index', {articles: articles})
+    } catch(e)
+    {
+        console.log(e)
+    }
+    
+} 
+)
+
+
+
+app.listen(process.env.PORT || 3000);
